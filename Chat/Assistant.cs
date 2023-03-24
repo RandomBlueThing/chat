eca539269ed74479c7378800bd54169aef398ef3
pj <pete.whitby@octavian.co.uk>
2023-03-24 16:42:52 +0000

    general tidy up

--- Chat/Assistant.cs
+++ Chat/Assistant.cs
@@ -1,11 +1,6 @@
 ﻿using OpenAI.GPT3.Interfaces;
 using OpenAI.GPT3.ObjectModels.RequestModels;
 using OpenAI.GPT3.ObjectModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chat;
 
@@ -14,15 +9,18 @@ internal class Assistant
     private readonly IOpenAIService _ai;
     private readonly Dictionary<string, Func<string, Task<bool>>> _commands;
     private readonly List<ChatMessage> _messages = new List<ChatMessage>();
+    private readonly List<ChatMessage> _prime = new List<ChatMessage>();
 
     public Assistant(IOpenAIService ai)
     {
         _ai = ai;
-        _commands = new Dictionary<string, Func<string, Task<bool>>>()
+        _commands = new Dictionary<string, Func<string, Task<bool>>>(StringComparer.OrdinalIgnoreCase)
         {
-            { "quit", Quit },
-            { "bye", Quit },
-            { "cfg", Cfg }
+            { "quit", p => Task.FromResult(true) },
+            { "bye", p => Task.FromResult(true) },
+            { "cfg", Cfg },
+            { "cls", p => { Console.Clear(); return Task.FromResult(false); } },
+            { "", p => Task.FromResult(false) }
         };
     }
 
@@ -30,24 +28,33 @@ internal class Assistant
     public async Task RunAsync()
     {
         // Prime history
-        _messages.AddRange(new[] {
-            new ChatMessage(StaticValues.ChatMessageRoles.System, "You are a helpful, slightly sarcastic, assistant called bob with a cat fixation. You talk about cats all the time.")
+        _prime.AddRange(new[] {
+            new ChatMessage(StaticValues.ChatMessageRoles.System, "You are a helpful, slightly sarcastic, assistant with a random name and cat fixation. You talk about cats all the time."),
+            new ChatMessage(StaticValues.ChatMessageRoles.System, "You have an imaginary cat whos name changes each time you mention them.")
         });
 
         while (true)
         {
-            Console.WriteLine();
-            Console.Write(">");
-
-            var prompt = Console.ReadLine();
-
-            if (prompt is not null && await ProcessAsync(prompt))
+            var prompt = ReadPrompt();
+            if(await ProcessAsync(prompt))
             {
                 break;
             }
         }
     }
 
+
+    private static string ReadPrompt()
+    {
+        Console.WriteLine();
+        //var c = Console.ForegroundColor;
+        //Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("> ");
+
+        var prompt = Console.ReadLine();
+        //Console.ForegroundColor = c;
+        return prompt??"";
+    }
 
 
     private Task<bool> ProcessAsync(string prompt)
@@ -60,26 +67,16 @@ internal class Assistant
     }
 
 
-    private Task<bool> Quit(string arg)
-    {
-        return Task.FromResult(true);
-    }
-
-    private Task<bool> Cfg(string arg)
-    {
-        Console.WriteLine("TODO: Cfg stuff");
-        return Task.FromResult(false);
-    }
-
-
     private async Task<bool> ProcessChatAsync(string prompt)
     {
         _messages.Add(new ChatMessage(StaticValues.ChatMessageRoles.User, prompt));
 
+        var messages = _prime.Concat(_messages).ToList();
+
         // @TODO: Instead of pushing entire history up, just push initial prime stuff & all *recent* stuff...
         var completionResult = _ai.ChatCompletion.CreateCompletionAsStream(new ChatCompletionCreateRequest
         {
-            Messages = _messages,
+            Messages = messages,
             Model = Models.ChatGpt3_5Turbo
         });
 
@@ -113,5 +110,12 @@ internal class Assistant
         _messages.Add(new ChatMessage(StaticValues.ChatMessageRoles.Assistant, rs));
 
         return false;
+    }
+
+    
+    private Task<bool> Cfg(string arg)
+    {
+        Console.WriteLine("TODO: Cfg stuff");
+        return Task.FromResult(false);
     }
 }
